362f5aff6c671e6a2bbe00f96edf60d5015d14e6
Nittya Gopal Baidya <72195264+nittya-gopal-baidya@users.noreply.github.com>
2025-03-02 13:34:49 +0530

    Delete user-form directory

--- user-form/back-end/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.repository;
-
-import com.example.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    // No need to define a custom create method; save() is already available
-    User findByEmail(String email);
-}
